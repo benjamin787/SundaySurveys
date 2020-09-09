23eea4a64520451970a3bfb62b7215a159c9065c
Ben Rowe <brow495@gmail.com>
2020-09-09 02:55:35 -0600

    Install react-hook-forms. Create post fetch to record question responses. Create checkboxes.

--- survey-app/src/components/SurveyPage.js
+++ survey-app/src/components/SurveyPage.js
@@ -1,16 +1,45 @@
 import React from 'react'
+import { useForm } from 'react-hook-forms'
+
+const responseURL = 'http://localhost:3000/question_responses'
 
 function SurveyPage({ survey }) {
 
-    
+    const { register, handleSubmit, getValue } = useForm()
+
+    const displayQuestions = () => {
+        survey.survey_questions.map(question => {
+            return ( 
+                <li className='question-li' key={question.id}>
+                    <label>{question.content}</label>
+                    <input name={question.id} type='checkbox' ref={register({required: true})} />
+                </li>
+            )
+        })
+    }
+
+    const submitAnswers = async () => {
+        const answers = getValue()
+        const promise = await fetch(responseURL, {
+            method: 'POST',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify(answers)
+        })
+        const confirmation = await promise.json()
+        // bubble appears either confirming/thanking or telling to fix something
+
+
+    }
     
     return (
         <div>
-            <ol className='questions-list'>
-                {survey.survey_questions.map(question => {
-                    return <li className='question-li' key={question.id}>{question.content}</li>
-                })}
-            </ol>        
+            <h2>{survey.title}</h2>
+            <form>
+                <ol className='questions-list'>
+                    {displayQuestions()}
+                </ol>
+                <button type='submit' onSubmit={handleSubmit(submitAnswers)}>Submit</button>
+            </form>
         </div>
     )
 }
